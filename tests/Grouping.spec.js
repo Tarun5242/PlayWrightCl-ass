a7ee4494718493450a9147457f95e06d1baf6b35
Tarun5242 <tarunsirvi1995@gmail.com>
2025-10-05 10:47:01 +0530

    grouping

---
+++ tests/Grouping.spec.js
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test'
+
+
+test.describe('Group 1', () => {
+
+    test('Grouping the tests', async ({ page }) => {
+        console.log('test 1')
+    })
+
+    test('Grouping the tests 2', async ({ page }) => {
+        console.log('test 2')
+    })
+})
+
+test.describe('Group 2', () => {
+
+    test('Grouping the tests 3', async ({ page }) => {
+        console.log('test 3')
+    })
+
+    test('Grouping the tests 4', async ({ page }) => {
+        console.log('test 4')
+    })
+})
+
+
+
